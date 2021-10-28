2b2285be3e37916c00af86f82ed77c7aac8fa4bb
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-10-28 17:38:33 +0200

    Install Prettier and prettify js files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css"
 
 function App() {
-  return (
-    <div>
-      <h1>Hello Find your Wine</h1>
-    </div>
-  );
+   return (
+      <div>
+         <h1>Hello Find your Wine</h1>
+      </div>
+   )
 }
 
-export default App;
+export default App
