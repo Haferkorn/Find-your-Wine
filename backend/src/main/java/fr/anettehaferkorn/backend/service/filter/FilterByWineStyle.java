4ef04f55a17d920bc753625cca898cdde099e76e
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-11-05 16:03:07 +0100

    Add Possibility To choose more than one wineStyle

---
+++ backend/src/main/java/fr/anettehaferkorn/backend/service/filter/FilterByWineStyle.java
@@ -0,0 +1,35 @@
+package fr.anettehaferkorn.backend.service.filter;
+
+import fr.anettehaferkorn.backend.model.RecommendationDTO;
+import fr.anettehaferkorn.backend.model.WineGrape;
+import fr.anettehaferkorn.backend.model.WineQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class FilterByWineStyle {
+    public static List<WineGrape> filteringByWineStyle(List<WineGrape> matchingWineByOccasion, WineQuery wineQuery) {
+        List<String> wineStyles=checkForMultipleWineStyles(wineQuery);
+        List<WineGrape>filteredWineGrapes=new ArrayList<>();
+
+        for (String wineStyle: wineStyles) {
+            for (WineGrape wineGrape : matchingWineByOccasion) {
+                if (Objects.equals(wineGrape.getWineStyle(), wineStyle)) {
+                    filteredWineGrapes.add(wineGrape);
+                }
+            }
+        }
+        return filteredWineGrapes;
+    }
+
+    private static List<String> splitPossibleStyles(String wineStyles){
+        return List.of(wineStyles.split(","));
+    }
+
+    private static List<String> checkForMultipleWineStyles(WineQuery wineQuery){
+        if(wineQuery.getWineStyle().contains(",")){
+           return splitPossibleStyles(wineQuery.getWineStyle());
+        }return List.of(wineQuery.getWineStyle());
+    }
+}
