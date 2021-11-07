57a2056116f8fd257f6f86762bce190b8ba570d4
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-11-07 16:16:16 +0100

    Change Integration Test

--- backend/src/test/java/fr/anettehaferkorn/backend/controller/RecommendationControllerTest.java
+++ backend/src/test/java/fr/anettehaferkorn/backend/controller/RecommendationControllerTest.java
@@ -1,7 +1,9 @@
 package fr.anettehaferkorn.backend.controller;
 
 import fr.anettehaferkorn.backend.model.RecommendationDTO;
+import fr.anettehaferkorn.backend.model.WineGrape;
 import fr.anettehaferkorn.backend.model.WineQuery;
+import fr.anettehaferkorn.backend.repo.WineGrapeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,12 +12,18 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecommendationControllerTest {
+
+        WineGrapeRepository wineGrapeRepository= mock(WineGrapeRepository.class);
 
         @LocalServerPort
         private int port;
@@ -26,8 +34,17 @@ class RecommendationControllerTest {
         void getRecommendation() {
             //Given
             String URL = "http://localhost:" + port + "/recommendation";
+            WineQuery wineQuery= new WineQuery("dinner","white","idK","light","dry");
+            //Mock
+            List<WineGrape>mockedRecommendations=List.of(
+                    new WineGrape("Merlot","dinner","red","France","light","sweet","abcd"),
+                    new WineGrape("BBBB","dinner","red","France","strong","dry","abcd"),
+                    new WineGrape("CCCC","dinner","red","France","strong","sweet","abcd")
+            );
+            when(wineGrapeRepository.findWineQueryDTOByOccasion(wineQuery.getOccasion())).thenReturn(mockedRecommendations);
+
             //When
-            ResponseEntity<RecommendationDTO[]> response = testRestTemplate.postForEntity(URL, new WineQuery("dinner","white","idK","light","dry"), RecommendationDTO[].class);
+            ResponseEntity<RecommendationDTO[]> response = testRestTemplate.postForEntity(URL, wineQuery, RecommendationDTO[].class);
             //Then
             assertThat(response.getStatusCode(), is(HttpStatus.OK));
 
@@ -37,8 +54,18 @@ class RecommendationControllerTest {
     void getRecommendation_With_Multiple_WineStyles() {
         //Given
         String URL = "http://localhost:" + port + "/recommendation";
+        WineQuery wineQuery= new WineQuery("dinner","white","idK","light","dry");
+        //Mock
+        List<WineGrape>mockedRecommendations=List.of(
+                new WineGrape("Merlot","dinner","red","France","light","sweet","abcd"),
+                new WineGrape("BBBB","dinner","red","France","strong","dry","abcd"),
+                new WineGrape("CCCC","dinner","red","France","strong","sweet","abcd")
+        );
+        when(wineGrapeRepository.findWineQueryDTOByOccasion(wineQuery.getOccasion())).thenReturn(mockedRecommendations);
+
+
         //When
-        ResponseEntity<RecommendationDTO[]> response = testRestTemplate.postForEntity(URL, new WineQuery("dinner","white,red","idK","light","dry"), RecommendationDTO[].class);
+        ResponseEntity<RecommendationDTO[]> response = testRestTemplate.postForEntity(URL, wineQuery, RecommendationDTO[].class);
         //Then
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
 
