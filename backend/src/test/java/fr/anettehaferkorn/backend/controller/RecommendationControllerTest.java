2f81acc0366a4e172cfcb7ae7cbf2cf15f9fd53b
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-11-08 16:05:31 +0100

    Change ControllerTest and Add TestDB

--- backend/src/test/java/fr/anettehaferkorn/backend/controller/RecommendationControllerTest.java
+++ backend/src/test/java/fr/anettehaferkorn/backend/controller/RecommendationControllerTest.java
@@ -4,6 +4,8 @@ import fr.anettehaferkorn.backend.model.RecommendationDTO;
 import fr.anettehaferkorn.backend.model.WineGrape;
 import fr.anettehaferkorn.backend.model.WineQuery;
 import fr.anettehaferkorn.backend.repo.WineGrapeRepository;
+import fr.anettehaferkorn.backend.service.DBService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,25 +18,42 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecommendationControllerTest {
-
-        WineGrapeRepository wineGrapeRepository= mock(WineGrapeRepository.class);
 
         @LocalServerPort
         private int port;
         @Autowired
         private TestRestTemplate testRestTemplate;
 
+        @Autowired
+        DBService dbService;
+
+        @Autowired
+        WineGrapeRepository wineGrapeRepository;
+
+        @BeforeEach
+        public void clearDb() {
+            wineGrapeRepository.deleteAll();
+        }
+
         @Test
         void getRecommendation() {
+            //Fill Data into DB
+            List<WineGrape> mockedRecommendations=List.of(
+                    new WineGrape("Merlot","dinner","red","France","light","sweet","abcd"),
+                    new WineGrape("BBBB","dinner","white","France","strong","dry","abcd"),
+                    new WineGrape("CCCC","dinner","red","France","strong","sweet","abcd")
+            );
+
+            dbService.saveToDB(mockedRecommendations);
+
+
             //Given
             String URL = "http://localhost:" + port + "/recommendation";
-            WineQuery wineQuery= new WineQuery("dinner","white","idK","light","dry");
+            WineQuery wineQuery= new WineQuery("dinner","red","France","strong","sweet");
 
             //When
             ResponseEntity<RecommendationDTO[]> response = testRestTemplate.postForEntity(URL, wineQuery, RecommendationDTO[].class);
@@ -45,6 +64,16 @@ class RecommendationControllerTest {
 
     @Test
     void getRecommendation_With_Multiple_WineStyles() {
+        //Fill Data into DB
+        List<WineGrape> mockedRecommendations=List.of(
+                new WineGrape("Merlot","dinner","red","France","light","sweet","abcd"),
+                new WineGrape("BBBB","dinner","white","France","strong","dry","abcd"),
+                new WineGrape("CCCC","dinner","red","France","strong","sweet","abcd")
+        );
+
+        dbService.saveToDB(mockedRecommendations);
+
+
         //Given
         String URL = "http://localhost:" + port + "/recommendation";
         WineQuery wineQuery= new WineQuery("dinner","white","idK","light","dry");
