59b380352fa71b05285ac5b4ebe3dd025f6e4e80
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-11-10 15:04:30 +0100

    Rename res to response

--- frontend/src/utils/AuthProvider.js
+++ frontend/src/utils/AuthProvider.js
@@ -5,20 +5,20 @@ import { useHistory } from 'react-router-dom'
 export const AuthContext = createContext({})
 
 export default function AuthProvider({ children }) {
-    const [token, setToken] = useState()
+    const [token, setToken] = useState("")
     const history = useHistory()
 
     const login = credentials => {
         axios
             .post('/auth/login', credentials)
-            .then(res => res.data)
+            .then(response => response.data)
             .then(setToken)
             .then(() => history.push('/'))
             .catch(error => console.error(error.message))
     }
 
     return (
-        <AuthContext.Provider value={{ token, login, loginWithGithub }}>
+        <AuthContext.Provider value={{ token, login}}>
             {children}
         </AuthContext.Provider>
     )
