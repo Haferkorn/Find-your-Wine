3f95f88e72a7258d8e98159d3eabbaebd5d50164
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-11-14 23:19:54 +0100

    Backend Fixes

--- backend/src/main/java/fr/anettehaferkorn/backend/BackendApplication.java
+++ backend/src/main/java/fr/anettehaferkorn/backend/BackendApplication.java
@@ -1,11 +1,8 @@
 package fr.anettehaferkorn.backend;
 
-import fr.anettehaferkorn.backend.security.model.AppUserDTO;
-import fr.anettehaferkorn.backend.security.repo.AppUserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class BackendApplication {
