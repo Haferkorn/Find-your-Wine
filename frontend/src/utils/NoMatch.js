a6b259961c82afd5325ccccd63265c2c89d5bd0e
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-11-24 12:41:50 +0100

    Refactor NoMatch component

--- frontend/src/utils/NoMatch.js
+++ frontend/src/utils/NoMatch.js
@@ -1,8 +1,8 @@
-import { Heading } from "./Recommendations"
+import { Heading } from "../components/pages/recommandations/Recommendations"
 import NoMatch_Animation from "./NoMatch_Animation.json"
 import { Controls, Player } from "@lottiefiles/react-lottie-player"
 
-import { AnimationWrapper } from "../homepage/Homepage"
+import { AnimationWrapper } from "../components/pages/homepage/Homepage"
 import styled from "styled-components/macro";
 
 function NoMatch() {
